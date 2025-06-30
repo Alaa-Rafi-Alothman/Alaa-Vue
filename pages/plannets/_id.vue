be97cffcb4282df3321b3f686f0c1d53dadb2043
Alaa-Rafi-Alothman <alaa.r.alothman@gmail.com>
2025-06-30 15:42:37 +0300

    commit 3

--- pages/plannets/_id.vue
+++ pages/plannets/_id.vue
@@ -1,11 +1,23 @@
 <template>
   <v-container>
     <h2>Hello Planet {{ this.$route.params.id }}</h2>
+
+
+  
+  <button @click="goBack"> <v-icon>mdi-arrow-left</v-icon></button>
+
   </v-container>
 </template>
 
 <script>
+
   export default {
     name: "PlannetsPage",
+
+  methods: {
+    goBack() {
+      this.$router.go(-1); // Goes one step back in browser history
+    }
   }
+}
 </script>
